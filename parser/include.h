1d97723bedae3bef272fd094fc9bea8589bfda12
Kostia Ilnytskyi <ilnkostia@gmail.com>
2023-07-12 19:50:19 +0200

    Complete rework🔧😁

--- parser/include.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSER_H
-#define PARSER_H
-
-#include "../utils/include.h"
-
-Vector* parse(ErrorGroup* error,MemoryGroup* token_memory,MemoryGroup* memory,Vector* tokens,int type, char* scope);
-
-
-#endif  
--- parser/token/export.c
+++
@@ -1,8 +0,0 @@
-#include "../private.h"
-
-// just export 
-void parser_token_export(Parser* parser){
-    parser->cmd->actual_type = PUB;
-    // clean up
-    parser_free_token(parser);
-}
