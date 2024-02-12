e4911685841e8f8714e5a190e104c47ad6dba451
Kostia Ilnytskyi <ilnkostia@gmail.com>
2024-02-11 21:11:36 -0700

    complete rework

--- src1/front/parser/main.cpp
+++ src1/front/parser/main.cpp
@@ -3,4 +3,27 @@
 
 Parser::Parser(Compiler* compiler): compiler(compiler){
     this->compiler = compiler; 
+    
+    std::vector<std::vector<Node*>> cmds;
+    std::vector<Node*>> current_cmd;
+    size_t index = 0;
+    while(index < this->compiler->nodes.size()){
+        Node* node = this->compiler->nodes[index];
+        cmds.push_back(node);
+        if(node->type == NodeType::NEW_LINE){
+            cmds.push_back(current_cmd);
+            current_cmd.clear();
+        }
+        index++;
+    }
+    
+    for(int i = 0; i < cmds.size(); i++){
+        std::vector<Node*> cmd = cmds[i];
+        for(int j = 0; j < cmd.size(); j++){
+            Node* node = cmd[j];
+            std::cout << node->type << " ";
+        }
+        std::cout << std::endl;
+    }
+
 }
